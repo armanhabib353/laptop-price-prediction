c4fb3bfadade2d905b7424d6af5f4e0aa5bfe719
Arman Asha <armanhabib353@gmail.com>
2023-08-16 14:17:39 +0600

    new

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import pandas as pd
 import numpy as np
 import pickle
 
+
 # file1 = open('pipe.pkl', 'rb')
 # rf = pickle.load(file1)
 # file1.close()
+
 
 # Apple,Ultrabook,8,Mac,1.37,0,1,226.98300468106115,Intel Core i5,0,128,Intel
 

b677fb4b27f8296c8bb48b3e33a5dd40d846be29
Arman Asha <armanhabib353@gmail.com>
2023-08-16 13:41:17 +0600

    new

--- app.py
+++ app.py
@@ -3,42 +3,93 @@ import pandas as pd
 import numpy as np
 import pickle
 
-# Load the pickled model
 file1 = open('pipe.pkl', 'rb')
 rf = pickle.load(file1)
 file1.close()
 
-# Load data used for training
+# Apple,Ultrabook,8,Mac,1.37,0,1,226.98300468106115,Intel Core i5,0,128,Intel
+
 data = pd.read_csv("traineddata.csv")
 
-st.title("Laptop Price Predictors")
+data['IPS'].unique()
 
-# Rest of your code...
+st.title("Laptop Price Predictor")
 
-# Inside the prediction button click block
+company = st.selectbox('Brand', data['Company'].unique())
+
+
+
+# type of laptop
+
+type = st.selectbox('Type', data['TypeName'].unique())
+
+# Ram present in laptop
+
+ram = st.selectbox('Ram(in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
+
+# os of laptop
+
+os = st.selectbox('OS', data['OpSys'].unique())
+
+# weight of laptop
+
+weight = st.number_input('Weight of the laptop')
+
+# touchscreen available in laptop or not
+
+touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
+
+# IPS
+
+ips = st.selectbox('IPS', ['No', 'Yes'])
+
+# screen size
+
+screen_size = st.number_input('Screen Size')
+
+# resolution of laptop
+
+resolution = st.selectbox('Screen Resolution', [
+                          '1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800', '2880x1800', '2560x1600', '2560x1440', '2304x1440'])
+
+# cpu
+
+cpu = st.selectbox('CPU', data['CPU_name'].unique())
+
+# hdd
+
+hdd = st.selectbox('HDD(in GB)', [0, 128, 256, 512, 1024, 2048])
+
+# ssd
+
+ssd = st.selectbox('SSD(in GB)', [0, 8, 128, 256, 512, 1024])
+
+gpu = st.selectbox('GPU(in GB)', data['Gpu brand'].unique())
+
 if st.button('Predict Price'):
-    try:
-        ppi = None
-        # Convert 'Yes'/'No' to 1/0
-        touchscreen = 1 if touchscreen == 'Yes' else 0
-        ips = 1 if ips == 'Yes' else 0
 
-        X_resolution = int(resolution.split('x')[0])
-        Y_resolution = int(resolution.split('x')[1])
+    ppi = None
+    if touchscreen == 'Yes':
+        touchscreen = 1
+    else:
+        touchscreen = 0
 
-        ppi = ((X_resolution ** 2) + (Y_resolution ** 2)) ** 0.5 / screen_size
+    if ips == 'Yes':
+        ips = 1
+    else:
+        ips = 0
 
-        # Ensure the order of features matches the training data
-        query = np.array([company, type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os])
+    X_resolution = int(resolution.split('x')[0])
+    Y_resolution = int(resolution.split('x')[1])
 
-        # Reshape the query array to match the expected shape
-        query = query.reshape(1, -1)
+    ppi = ((X_resolution**2)+(Y_resolution**2))**0.5/(screen_size)
 
-        # Make the prediction
-        prediction = int(np.exp(rf.predict(query)[0]))
+    query = np.array([company, type, ram, weight,
+                      touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os])
 
-        st.title("Predicted price for this laptop could be between " +
-                 str(prediction - 1000) + "₹" + " to " + str(prediction + 1000) + "₹")
+    query = query.reshape(1, 12)
 
-    except Exception as e:
-        st.error("An error occurred during prediction: " + str(e))
+    prediction = int(np.exp(rf.predict(query)[0]))
+
+    st.title("Predicted price for this laptop could be between " +
+             str(prediction-1000)+"₹" + " to " + str(prediction+1000)+"₹")
